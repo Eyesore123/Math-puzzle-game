5b4df1c3b0c63bb70cd0e4a0b18d8259f0849f2d
Eyesore123 <joni.putkinen@protonmail.com>
2025-02-13 08:56:57 +0200

    added a landing page footer

--- src/assets/pages/Welcomescreen.tsx
+++ src/assets/pages/Welcomescreen.tsx
@@ -1,7 +1,21 @@
 import '../../index.css';
 import './Welcomescreen.css';
+import './Landingpagefooter.css';
+import GithubIcon from '../Github.png';
+
+import { useEffect } from 'react';
 
 export default function Welcomescreen() {
+
+  useEffect(() => {
+    const landingpagefooter = document.getElementById('landingpagefooter');
+    if (landingpagefooter) {
+      landingpagefooter.addEventListener('click', function() {
+        landingpagefooter.style.display = 'none';
+      });
+    }
+  }, []);
+
   return (
     <div>
       <h1>Welcome to Math Puzzle Game!</h1>
@@ -15,6 +29,24 @@ export default function Welcomescreen() {
         </p>
       <div className='instructions'>Click here to get started:</div>
       <button onClick={() => window.location.href = '/game'}>Start</button>
+      
+      <footer className="landingpagefooter" id="landingpagefooter" style={{ display: 'block' }}>
+            <div className="footerdiv" id="footerdiv">
+                <p className="game1" style={{ color: 'black'}} id="game1">Check out my other games:
+                </p>
+                <p className="game2" id="game2">
+                    <a href="https://memory-game-topaz-pi.vercel.app/" target="_blank">Memory Game</a>
+                </p>
+                <p className="game3" id="game3">
+                    <a href="https://snakegame-mocha-kappa.vercel.app/" target="_blank">Snake Game</a>
+                </p>
+                <p className="github" id="github">
+                    <a href="https://github.com/Eyesore123" target="_blank">
+                    <img src={GithubIcon} alt="Github Icon" className="githubicon" id="githubicon" />
+                    </a>
+                </p>
+            </div>
+        </footer>
     </div>
   )
 }
